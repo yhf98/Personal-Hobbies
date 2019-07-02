1ae4db2e7825fa936e7d848a7f62bb82f10fbf6c
angular <1921934563@qq.com>
2019-07-03 00:59:20 +0800

    匿名类型 one

--- EF多表查询/Controllers/HomeController.cs
+++ EF多表查询/Controllers/HomeController.cs
@@ -12,24 +12,47 @@ namespace EF多表查询.Controllers
     {
         public ActionResult Index()
         {
+            //using (StuEntities db = new StuEntities())
+            //{
+            //    var userListTest = (from u in db.Class
+            //                        join p in db.Stu on u.ClassID equals p.ClassID
+            //                        select new {StuID=p.StuID,StuName=p.StuName,ClassName=u.ClassName });
+            //    List<dynamic> oneList = new List<dynamic>();
+            //    foreach (var one in userListTest.ToList())
+            //    {
+            //        dynamic dyObject = new ExpandoObject();
+            //        dyObject.StuName = one.StuName;
+            //        dyObject.StuID = one.StuID;
+            //        dyObject.ClassName = one.ClassName;
+            //        oneList.Add(dyObject);
+            //    }
+
+            //    ViewBag.dyObject = oneList;
+            //    return View();
+            //}   
+
             using (StuEntities db = new StuEntities())
             {
-                var userListTest = (from u in db.Class
-                                    join p in db.Stu on u.ClassID equals p.ClassID
-                                    select new {StuID=p.StuID,StuName=p.StuName,ClassName=u.ClassName });
-                List<dynamic> oneList = new List<dynamic>();
-                foreach (var one in userListTest.ToList())
+                var StuList = from c in db.Class
+                           join s in db.Stu on c.ClassID equals s.ClassID
+                           select new {StuID=s.StuID,StuName=s.StuName,ClassName=c.ClassName };
+
+                List<dynamic> list = new List<dynamic>();
+
+                foreach (var t in StuList.ToList())
                 {
-                    dynamic dyObject = new ExpandoObject();
-                    dyObject.StuName = one.StuName;
-                    dyObject.StuID = one.StuID;
-                    dyObject.ClassName = one.ClassName;
-                    oneList.Add(dyObject);
+                    dynamic d = new ExpandoObject();
+                    d.StuID = t.StuID;
+                    d.StuName = t.StuName;
+                    d.ClassName = t.ClassName;
+                    list.Add(d);
                 }
 
-                ViewBag.dyObject = oneList;
+                ViewBag.list = list;
+
                 return View();
-            }   
+
+            }
         }
 
         public ActionResult About()
