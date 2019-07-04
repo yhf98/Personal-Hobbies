27a44bdb6cf6b26a6ca369d1aed3e159d95a9b16
angular <1921934563@qq.com>
2019-07-04 22:42:19 +0800

    特征

---
+++ XMLHttpRequest/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace XMLHttpRequest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AA(string name,string pwd)
+        {
+            var re = new { total=10,rows=new string[] {"AA","SS","DD","FF","GG" } };
+            return Json(re,JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ XMLHttpRequest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="XMLHttpRequest.MvcApplication" Language="C#" %>
